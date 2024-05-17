4a2fcfe4331869be25951d3cefaff3f182828ea2
Myroslav Yaremenko <ymiroshavs@gmail.com>
2024-05-16 23:19:40 -0500

    vpc

--- vpc-task/vpc-new/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    required_version = "1.8.3"     // Terraform Version
-    required_providers {     // Provider Version  
-      aws = {
-        source = "hashicorp/aws"
-        version = "5.49.0"
-      }
-    }
-}
